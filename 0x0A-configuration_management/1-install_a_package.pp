4b4b6524905c6079be1f4bedab49090336cadd37
hamdouditarek <hamdouditarek@gmail.com>
2024-03-23 11:32:04 +0100

    all

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #!/usr/bin/pup
-# Install a specific version of flask (2.1.0)
-package {'flask':
+# Install Flask 2.1.0 using pip3
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'gem'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
